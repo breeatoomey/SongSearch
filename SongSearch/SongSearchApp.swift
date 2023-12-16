fec32bf0bff80b7ef3d774f86e7cbe5cf90a0db7
Breea Toomey <breeatoomey@gmail.com>
2023-12-15 23:41:56 -0800

    initial commit

---
+++ SongSearch/SongSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongSearchApp.swift
+//  SongSearch
+//
+//  Created by Breea Toomey on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SongSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
